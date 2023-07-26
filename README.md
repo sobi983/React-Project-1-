878b9a5b87672f744205e4c658be0127131dd073
unknown <sohai@Sobi.ncutech.com>
2023-07-26 08:43:56 +0300

    EndGame feature added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 In this project I am creating a game named Tenzies
+![Alt Text](/project-image.png)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import { useState } from "react";
-// import { nanoid } from 'nanoid'
-import Dice from "./components/dice";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
+import ConfettiExplosion from 'react-confetti-explosion';
+import Confetti from 'react-confetti'
+import Dice from "./components/dice";
 
 function App() {
-  let [diceNumber, setDiceNumber] = useState(fillDice())
+  let [diceNumber, setDiceNumber] = useState(fillDice)
   let [selectedNumber, setSelectedNumber] = useState(null)
+  let [decline, setDecline] = useState(false)
+  let [isExploding, setIsExploding] = useState(false);
+  // let [winnerCount, setWinnerCount] = useState(1)
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  useEffect(() => {
+    let winner = diceNumber.every((check) => check.isHold === true)
+    if (winner === true) {
+      setIsExploding(true)
+    }
+  }, [diceNumber])
 
   function fillDice() {
     let newArr = []
     for (let i = 1; i <= 10; i++) {
-      newArr.push(genrateDice())
+      newArr.push(generateDice())
     }
     return newArr;
   }
 
-  function genrateDice() {
+  function generateDice() {
     let obj = {
       id: nanoid(),
       isHold: false,
@@ -23,19 +36,31 @@ function App() {
     }
     return obj
   }
-  
+
   function onHold(id) {
     setDiceNumber(prev => {
       return prev.map(items => {
         if (id === items.id) {
           if (selectedNumber === null) {
-            setSelectedNumber(prev=> items.number)
+            setDecline(false)
+            setSelectedNumber(items.number)
+            // setWinnerCount(prev => prev + 1)
             return { ...items, isHold: !items.isHold }
           }
-          else if (selectedNumber === items.number) {
+          else if (selectedNumber === items.number && items.isHold === false) {
+            setDecline(false)
+            // setWinnerCount(prev => prev + 1)
             return { ...items, isHold: !items.isHold }
           }
-          else if(selectedNumber !== items.number){
+          else if (selectedNumber === items.number && items.isHold === true) {
+            // setWinnerCount(prev => prev - 1)
+            setDecline(false)
+            setSelectedNumber(null)
+            setIsExploding(false)
+            return { ...items, isHold: !items.isHold }
+          }
+          else if (selectedNumber !== items.number) {
+            setDecline(true)
             return items
           }
         }
@@ -57,13 +82,16 @@ function App() {
   function roll() {
     setDiceNumber(function (prev) {
       return prev.map(function (items) {
-        return items.isHold === true ? { ...items } : genrateDice()
+        return items.isHold === true ? { ...items } : generateDice()
       })
     })
   }
 
-
-
+  function startAgain(){
+    setDiceNumber(fillDice())
+    setSelectedNumber(null)
+    setIsExploding(false)
+  }
 
   return (
     <div className="container mx-auto bg-slate-900/95 p-8 sm:p-12 h-screen">
@@ -71,11 +99,21 @@ function App() {
         <h1 className="text-gray-800 font-handjet font-extrabold  text-8xl sm:text-9xl mt-8">Tenzies</h1>
         <h3 className="text-slate-600 font-tektur text-center text-xl sm:text-2xl sm:w-6/12 ">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
 
+        {decline === true &&
+          <div className="border bg-red-500 p-5 mt-4">
+            <strong>Nope! </strong>
+            I won't allow that, Noobray
+          </div>
+        }
         <div className="grid grid-cols-5 grid-rows-2 mt-24 gap-6 sm:gap-12">
           {diceShow}
         </div>
 
-        <button className="mt-16 mb-10 rounded-r-lg border rounded text-4xl text-white bg-purple-900 p-4 px-12 shadow-xl hover:bg-purple-950" onClick={roll}>Roll</button>
+
+        {isExploding === false ? <button className="mt-16 mb-10 rounded-r-lg border rounded text-4xl text-white bg-purple-900 p-4 px-12 shadow-xl hover:bg-purple-950" onClick={roll}>Roll 
+        </button> : <button className="mt-16 mb-10 rounded-r-lg border rounded text-4xl text-white bg-purple-900 p-4 px-9 shadow-xl hover:bg-purple-950" onClick={startAgain}>Start Again 
+        </button>}
+        {isExploding === true && <Confetti width={width} height={height} />}
       </div>
     </div>
   );
